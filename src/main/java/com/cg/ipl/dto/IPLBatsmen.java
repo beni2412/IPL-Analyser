e769818b355050f7a91de069ea55ed508432d895
beni2412 <hardaman97@gmail.com>
2020-10-30 01:42:47 +0530

    [Hardaman] Added: POJO classes for batsmen and bowler

---
+++ src/main/java/com/cg/ipl/dto/IPLBatsmen.java
@@ -0,0 +1,106 @@
+package com.cg.ipl.dto;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IPLBatsmen {
+	@CsvBindByName(column = "POS")
+	private String position;
+
+	@CsvBindByName(column = "PLAYER")
+	private String player;
+
+	@CsvBindByName(column = "Mat")
+	private String matches;
+
+	@CsvBindByName(column = "inns")
+	private String innings;
+
+	@CsvBindByName(column = "NO")
+	private String notOuts;
+
+	@CsvBindByName(column = "Runs")
+	private String runs;
+
+	@CsvBindByName(column = "HS")
+	private String highestScore;
+
+	@CsvBindByName(column = "Avg")
+	private String avg;
+
+	@CsvBindByName(column = "BF")
+	private String ballsFaced;
+
+	@CsvBindByName(column = "SR")
+	private String strikeRate;
+
+	@CsvBindByName(column = "100")
+	private String hundreds;
+
+	@CsvBindByName(column = "50")
+	private String fifties;
+
+	@CsvBindByName(column = "4s")
+	private String fours;
+
+	@CsvBindByName(column = "6s")
+	private String sixes;
+
+	public int getPosition() {
+		return Integer.parseInt(position);
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public int getMatches() {
+		return Integer.parseInt(matches);
+	}
+
+	public int getInnings() {
+		return Integer.parseInt(innings);
+	}
+
+	public int getNotOuts() {
+		return Integer.parseInt(notOuts);
+	}
+
+	public int getRuns() {
+		return Integer.parseInt(runs);
+	}
+
+	public int getHighestScore() {
+		return Integer.parseInt(highestScore);
+	}
+
+	public double getAvg() {
+		if (avg.equals("-"))
+			return 0.0;
+		return Double.parseDouble(avg);
+	}
+
+	public int getBallsFaced() {
+		return Integer.parseInt(ballsFaced);
+	}
+
+	public double getStrikeRate() {
+		return Double.parseDouble(strikeRate);
+	}
+
+	public int getHundreds() {
+		return Integer.parseInt(hundreds);
+	}
+
+	public int getFifties() {
+		return Integer.parseInt(fifties);
+	}
+
+	public int getFours() {
+		return Integer.parseInt(fours);
+	}
+
+	public int getSixes() {
+		return Integer.parseInt(sixes);
+	}
+
+}
---
+++ src/main/java/com/cg/ipl/dto/IPLBowler.java
@@ -0,0 +1,100 @@
+package com.cg.ipl.dto;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IPLBowler {
+	@CsvBindByName(column = "POS")
+	public String pos;
+
+	@CsvBindByName(column = "PLAYER")
+	public String player;
+
+	@CsvBindByName(column = "Mat")
+	public String matches;
+
+	@CsvBindByName(column = "inns")
+	public String innings;
+
+	@CsvBindByName(column = "Ov")
+	public String overs;
+
+	@CsvBindByName(column = "Runs")
+	public String runs;
+
+	@CsvBindByName(column = "Wkts")
+	public String wickets;
+
+	@CsvBindByName(column = "BBI")
+	public String bestBowling;
+
+	@CsvBindByName(column = "Avg")
+	public String avg;
+
+	@CsvBindByName(column = "Econ")
+	public String economyRate;
+
+	@CsvBindByName(column = "SR")
+	public String strikeRate;
+
+	@CsvBindByName(column = "4w")
+	public String fourWickets;
+
+	@CsvBindByName(column = "5w")
+	public String fiveWickets;
+
+	public int getPos() {
+		return Integer.parseInt(pos);
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public int getMatches() {
+		return Integer.parseInt(matches);
+	}
+
+	public int getInnings() {
+		return Integer.parseInt(innings);
+	}
+
+	public double getOvers() {
+		return Double.parseDouble(overs);
+	}
+
+	public int getRuns() {
+		return Integer.parseInt(runs);
+	}
+
+	public int getWickets() {
+		return Integer.parseInt(wickets);
+	}
+
+	public int getBestBowling() {
+		return Integer.parseInt(bestBowling);
+	}
+
+	public double getAvg() {
+		if (avg.equals("-"))
+			return 0.0;
+		return Double.parseDouble(avg);
+	}
+
+	public double getEconomyRate() {
+		return Double.parseDouble(economyRate);
+	}
+
+	public double getStrikeRate() {
+		if (strikeRate.equals("-"))
+			return 0.0;
+		return Double.parseDouble(strikeRate);
+	}
+
+	public int getFourWickets() {
+		return Integer.parseInt(fourWickets);
+	}
+
+	public int getFiveWickets() {
+		return Integer.parseInt(fiveWickets);
+	}
+}
